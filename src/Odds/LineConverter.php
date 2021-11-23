bff0903f941e17ffd224f765eb7792825d8a4118
Ben Rose <benr242@mail.com>
2021-11-22 21:22:14 -0500

    better, incomplete service

---
+++ src/Odds/LineConverter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Odds;
+
+class LineConverter
+{
+    protected int $fLine;
+    protected int $dLine;
+
+    protected float $fImpProb;
+    protected float $dImpProb;
+
+    protected float $fOdds;
+    protected float $dOdds;
+
+    public function __construct(int $fLine, int $dLine)
+    {
+        $this->fLine = $fLine;
+        $this->dLine = $dLine;
+    }
+
+    public function setLines(int $fLine, int $dLine)
+    {
+        $this->fLine = $fLine;
+        $this->dLine = $dLine;
+    }
+
+    public function setImpProb()
+    {
+        $this->fImpProb = $this->fLine/($this->fLine + 100) * 100;
+        $this->dImpProb = 100/($this->dLine + 100) * 100;
+    }
+
+    public function setOdds()
+    {
+        $this->fOdds = $this->fImpProb / ($this->fImpProb + $this->dImpProb);
+        $this->dOdds = $this->dImpProb / ($this->dImpProb + $this->fImpProb);
+    }
+}
