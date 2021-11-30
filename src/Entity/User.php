7145ea2f1a3dcc107425a431f14bdfabf618bbc8
Ben Rose <benr242@mail.com>
2021-11-29 22:10:20 -0500

    composer dump-autoload: helped error in making entities

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,39 +7,22 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
- */
+#[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private $id;
 
-    /**
-     * @ORM\Column(type="string", length=180, unique=true)
-     */
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $username;
 
-    /**
-     * @ORM\Column(type="json")
-     */
+    #[ORM\Column(type: 'json')]
     private $roles = [];
 
-    /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
-     */
+    #[ORM\Column(type: 'string')]
     private $password;
-
-    /**
-     * @ORM\Column(type="integer")
-     */
-    private $stash;
 
     public function getId(): ?int
     {
@@ -123,17 +106,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
-    }
-
-    public function getStash(): ?int
-    {
-        return $this->stash;
-    }
-
-    public function setStash(int $stash): self
-    {
-        $this->stash = $stash;
-
-        return $this;
     }
 }
